66ac92940e1b76be7962dd84afeb0b4f6c7b806c
Pankaj sharam <Pankajsharma1997>
2024-06-25 10:50:00 +0530

    add  Pagination in the report page

--- frontend/src/components/Admin/Report.js
+++ frontend/src/components/Admin/Report.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Table from "react-bootstrap/Table";
 import Sidebar from "./Sidebar";
+import ReactPaginate from "react-paginate";
+
 
 const Report = () => {
   const [items, setItems] = useState([]);
   const [companyName, setCompanyName] = useState("All");
+ 
 
   useEffect(() => {
     // This function will fetch items based on the selected company name
@@ -20,42 +23,49 @@ const Report = () => {
 
     fetchItems(); // Call the fetchItems function
   }, [companyName]); // This effect should run whenever companyName changes
+     
+  //  Pagination
+  const [pageNumber , setPageNumber] =  useState(0);
+  const postPerPage = 5;
+  const pagesVisited = pageNumber * postPerPage;
+  const pageCount = Math.ceil(items.length / postPerPage);
+  const changePage = ({selected}) => {
+        setPageNumber(selected);
+  }
 
   return (
     <div className="report">
-        <div className="row">
-          <div className="col-md-2">
-            <Sidebar />
-          </div>
-          <div className="container col-md-10 mt-5 mb-4 bg-light text-dark">
-            <Table responsive striped bordered hover>
-              <thead>
-                <tr>
-                  <th> Serial Number </th>
-                  {}
-                  <th>
-                    {" "}
-                    Select Company{" "}
-                    <select onChange={(e) => setCompanyName(e.target.value)}>
-                      <option value="All">All</option>
-                      {/* Map through your companies here */}
-                      <option value="Experts Solutions">
-                        Experts Solutions
-                      </option>
-                      <option value="Matrix Infologics">
-                        Matrix Infologics
-                      </option>
-                    </select>{" "}
-                  </th>
-                  {/* <th> Company Name </th> */}
-                  <th> Applicant Name </th>
-                  <th> Profile </th>
-                  <th> status </th>
-                </tr>
-              </thead>
-              <tbody>
-                <>
-                  {items.map((item, index) => (
+      <div className="row">
+        <div className="col-md-2">
+          <Sidebar />
+        </div>
+        <div className="container col-md-10 mt-5 mb-4 bg-light text-dark">
+          <Table responsive striped bordered hover>
+            <thead>
+              <tr>
+                <th> Serial Number </th>
+                {}
+                <th>
+                  {" "}
+                  Select Company{" "}
+                  <select onChange={(e) => setCompanyName(e.target.value)}>
+                    <option value="All">All</option>
+                    {/* Map through your companies here */}
+                    <option value="Experts Solutions">Experts Solutions</option>
+                    <option value="Matrix Infologics">Matrix Infologics</option>
+                  </select>{" "}
+                </th>
+                {/* <th> Company Name </th> */}
+                <th> Applicant Name </th>
+                <th> Profile </th>
+                <th> status </th>
+              </tr>
+            </thead>
+            <tbody>
+              <>
+                {items
+                  .slice(pagesVisited, pagesVisited + postPerPage)
+                  .map((item, index) => (
                     <tr key={index}>
                       <td>{index + 1} </td>
                       <td> {item.companyName} </td>
@@ -64,68 +74,79 @@ const Report = () => {
                       <td colSpan={2}> {item.status} </td>
                     </tr>
                   ))}
-                  <tr>
-                    {" "}
-                    <td colSpan={2}>Total Applicants: {items.length} </td>
-                    <td> </td>
-                    <td>
-                      <li>
-                        Developer:{" "}
-                        {
-                          items.filter(
-                            (applicant) =>
-                              applicant.jobTitle === "Web Developer"
-                          ).length
-                        }{" "}
-                      </li>
-                      <li>
-                        {" "}
-                        Designer:{" "}
-                        {
-                          items.filter(
-                            (applicant) => applicant.jobTitle === "Web Designer"
-                          ).length
-                        }
-                      </li>
+                <tr>
+                  {" "}
+                  <td colSpan={2}>Total Applicants: {items.length} </td>
+                  <td> </td>
+                  <td>
+                    <li>
+                      Developer:{" "}
+                      {
+                        items.filter(
+                          (applicant) => applicant.jobTitle === "Web Developer"
+                        ).length
+                      }{" "}
+                    </li>
+                    <li>
+                      {" "}
+                      Designer:{" "}
+                      {
+                        items.filter(
+                          (applicant) => applicant.jobTitle === "Web Designer"
+                        ).length
+                      }
+                    </li>
 
-                      <li>
-                        {" "}
-                        FullStack ={" "}
-                        {
-                          items.filter(
-                            (applicant) =>
-                              applicant.jobTitle === "Full-Stack-Developer"
-                          ).length
-                        }
-                      </li>
-                    </td>
-                    <td>
-                      <li>
-                        Accepted:{" "}
-                        {
-                          items.filter(
-                            (applicant) => applicant.status === "Accepted"
-                          ).length
-                        }{" "}
-                      </li>
-                      <li>
-                        {" "}
-                        Pending ={" "}
-                        {
-                          items.filter(
-                            (applicant) => applicant.status === "Pending"
-                          ).length
-                        }
-                      </li>
-                    </td>
-                  </tr>
-                </>
-              </tbody>
-            </Table>
-          </div>
+                    <li>
+                      {" "}
+                      FullStack ={" "}
+                      {
+                        items.filter(
+                          (applicant) =>
+                            applicant.jobTitle === "Full-Stack-Developer"
+                        ).length
+                      }
+                    </li>
+                  </td>
+                  <td>
+                    <li>
+                      Accepted:{" "}
+                      {
+                        items.filter(
+                          (applicant) => applicant.status === "Accepted"
+                        ).length
+                      }{" "}
+                    </li>
+                    <li>
+                      {" "}
+                      Pending ={" "}
+                      {
+                        items.filter(
+                          (applicant) => applicant.status === "Pending"
+                        ).length
+                      }
+                    </li>
+                  </td>
+                </tr>
+                <div>
+                  <ReactPaginate
+                    previousLabel={"Previous"}
+                    nextLabel={"Next"}
+                    pageCount={pageCount}
+                    onPageChange={changePage}
+                    containerClassName={"pagination"}
+                    previousLinkClassName={"pagination__link"}
+                    nextLinkClassName={"pagination__link"}
+                    disabledClassName={"pagination__link--disabled"}
+                    activeClassName={"pagination__link--active"}
+                  />
+                </div>
+              </>
+            </tbody>
+          </Table>
         </div>
       </div>
-    
+    </div>
   );
 };
 
